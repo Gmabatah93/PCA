4255b96bf94a451148dfa15d361bfb0ab8e5fe1a
Isioma Mabatah <Mabatah93@gmail.com>
2021-04-10 13:24:22 -0500

    PCA

--- pca.R
+++ pca.R
@@ -1,9 +1,12 @@
 library(readr)
 library(dplyr)
+library(tibble)
 library(FactoMineR)
 library(factoextra)
 
 # PCA: State ----
+
+# EDA
 
 # Matrix
 state_CovMatrix <- cov(scale(state.x77))
@@ -23,46 +26,62 @@ state_PCA %>% plot()
 # PCA: Decathon ----
 
 # DECATHOLON
+
 # Data
-decathlon2
-decathlon2 %>% skimr::skim()
+decathlon <- decathlon2 %>% 
+  rownames_to_column(var = "name") %>% as_tibble()
+decathlon_scores <- decathlon %>% 
+  select(X100m:X1500m)
 
-# EDA: pca
-dec_PCA <- PCA(decathlon2, graph = FALSE,
-               ind.sup = 24:27,
-               quanti.sup = 11:12,
-               quali.sup = 13)
-# - Eigenvalues
-fviz_eig(dec_PCA, addlabels = TRUE, ylim = c(0,50))
-# - extract results for variables
-pca_var <- dec_PCA %>% get_pca_var()
-# - visual
-dec_PCA$var$cos2 %>%  ggcorrplot::ggcorrplot(lab = TRUE)
-pca_ind <- dec_PCA %>% get_pca_ind()
+# EDA
+decathlon_scores %>% cor() %>% 
+  corrplot::corrplot(method = "number", type = "upper")
+  
+# Matrix
+decathlon_Matrix <- decathlon_scores %>% scale() %>% as.matrix()
+decathlon_CovMatrix <- decathlon_Matrix %>% cov()
 
+# Eigen
+decathlon_Eig <- decathlon_CovMatrix %>% eigen()
+decathlon_Eig$values
+decathlon_Eig$vectors
 
-# Variables
-fviz_pca_var(dec_PCA,
-             # Features
-             col.var = "black",
-             # Target
-             col.quanti.sup = "red")
-fviz_contrib(dec_PCA, choice = "var", axes = 1:2, top = 10)
+# PCA
+decathlon_PCA <- decathlon_scores %>% 
+  prcomp(center = TRUE, scale. = TRUE)
+decathlon_PCA$sdev^2
+decathlon_PCA$rotation
 
-# Observations
-fviz_pca_ind(dec_PCA, repel = TRUE,
-             # Test
-             col.ind.sup = "blue",
-             # Group
-             habillage = 13, palette = "jco",
-             addEllipses = TRUE, ellipse.type = "confidence"
-) %>% 
-  # Target
-  fviz_add(dec_PCA$quali.sup$coord, color = "red")
+# FactoM
+decathlon_FactoM <- decathlon %>%
+  select(X100m:X1500m) %>% 
+  PCA(scale.unit = TRUE, graph = FALSE)
+# - eigens
+decathlon_FactoM$svd$vs^2
+decathlon_FactoM$svd$V
+decathlon_FactoM %>% get_eig()
+decathlon_FactoM %>% fviz_eig(addlabels = TRUE)
 
-fviz_contrib(mod_pca, choice = "ind", axes = 1:2, top = 20)
+# Plot
+decathlon_FactoM %>% 
+  fviz_pca_biplot(repel = TRUE,
+                  col.var = "black", alpha.var = "cos2")
+# - variables
+decathlon_FactoM$var$coord[,c(1,2)]
+decathlon_FactoM$var$cos2[,c(1,2)] # Quality of Representation
+decathlon_FactoM$var$contrib[,c(1,2)]
 
+# - individuals:
+decathlon_FactoM$ind$coord[,c(1,2)]
+decathlon_FactoM$ind$cos2[,c(1,2)] # Quality of Representation
+decathlon_FactoM$ind$contrib[,c(1,2)]
 
+# PCA-Competition
+decathlon_FactoM %>% fviz_pca_biplot(repel = TRUE,
+                                     col.var = "black", alpha.var = "cos2",
+                                     col.ind = decathlon$Competition, addEllipses = TRUE, ellipse.type = "confidence")
+
+#
 # PCA: NFL Combine ----
 
 # Data
@@ -94,8 +113,16 @@ combine_PCA$var$
 
 # Data
 pokemon <- read_csv("Data/Pokemon.csv")
+pokemon <- pokemon %>% 
+  mutate(Type1 = Type1 %>% factor,
+         Type2 = Type2 %>% factor,
+         Generation = Generation %>% factor())
 pokemon_stats <- pokemon %>% 
   select(HitPoints:Speed)
+
+# EDA
+pokemon_stats %>% cor() %>% 
+  corrplot::corrplot(method = "number", type = "upper")
 
 # Matrix
 pokemon_Cov <- pokemon_stats %>% scale() %>% cov()
@@ -106,5 +133,50 @@ pokemon_Eig$values
 pokemon_Eig$vectors
 
 # PCA
-pokemon_PCA <- pokemon_stats %>% prcomp(scale = TRUE)
-pokemon_PCA
+pokemon_PCA_Base <- pokemon_stats %>% prcomp(scale = TRUE)
+# - eigenvalues
+pokemon_PCA_Base$sdev^2
+pokemon_PCA_Base$rotation
+
+# FactoM
+pokemon_PCA <- pokemon %>%
+  select(HitPoints:Speed) %>% 
+  PCA(scale.unit = TRUE, graph = FALSE)
+pokemon_PCA$svd$vs^2
+pokemon_PCA$svd$V
+pokemon_PCA %>% get_eig()
+pokemon_PCA %>% fviz_eig(addlabels = TRUE)
+
+pokemon_PCA %>% 
+  fviz_pca_biplot(repel = TRUE,
+                  col.var = "black", 
+                  alpha.ind = "cos2")
+# - variables
+pokemon_PCA$var$coord[,c(1,2)]
+pokemon_PCA$var$cos2[,c(1,2)] # Quality of Representation
+pokemon_PCA$var$contrib[,c(1,2)]
+
+# PCA-Legendary
+pokemon_PCA %>% fviz_pca_biplot(repel = TRUE,
+                                col.var = "black", alpha.ind = "cos2",
+                                col.ind = pokemon$Legendary, addEllipses = TRUE, ellipse.type = "confidence",
+                                legend.title = "Legendary")
+
+# PCA-Type 1
+pokemon_PCA %>% fviz_pca_biplot(repel = TRUE,
+                                col.var = "black", alpha.ind = "cos2",
+                                col.ind = pokemon$Type1, addEllipses = TRUE, ellipse.type = "confidence",
+                                legend.title = "Type 1")
+
+# PCA-Type 2 
+pokemon_PCA %>% fviz_pca_biplot(repel = TRUE,
+                                col.var = "black", alpha.ind = "cos2",
+                                col.ind = pokemon$Type2, addEllipses = TRUE, ellipse.type = "confidence",
+                                legend.title = "Type 2")
+
+# PCA-Generation
+pokemon_PCA %>% fviz_pca_biplot(repel = TRUE,
+                                col.var = "black", alpha.ind = "cos2",
+                                col.ind = pokemon$Generation, addEllipses = TRUE, ellipse.type = "confidence",
+                                legend.title = "Generation")
+
